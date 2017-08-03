71a191d322908ea05c94da438b0dbb76b1bd7fdd
saileshkasaju <leosailesh@gmail.com>
2017-08-04 00:31:20 +0545

    add monaco editor

--- renderer.js
+++ renderer.js
@@ -1,1 +1,10 @@
+const loader = require('monaco-loader')
 
+loader().then(monaco => {
+  let editor = monaco.editor.create(document.querySelector
+  ('#container'), {
+    language: 'javascript',
+    theme: 'vs-dark',
+    automaticLayout: true
+  })
+})
